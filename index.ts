7445e249d73b802cd7f692720b3c651c3c58f823
hooriya <parih3080@gmail.com>
2024-05-14 00:53:43 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,24 @@
+// QUESTION 118:
+for (let i = 1; i <= 10; i++) {
+  console.log(i);
+}
+
+// QUESTION 119:
+let numbers: number = 0;
+while (numbers < 5) {
+  console.log("Hello,World!!!");
+  numbers++;
+}
+
+// QUESTION 120:
+let arrayOfFavoriteMovies: string[] = [
+  "Disnep Beauty And The Beast",
+  "inception",
+  "The Dark Knight",
+  "Live Die Repeat",
+];
+for (let i= 0; i<arrayOfFavoriteMovies.length; i++) {
+  console.log(arrayOfFavoriteMovies[i]);
+    
+}
+
